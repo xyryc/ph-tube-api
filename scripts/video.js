5d76aeb27bde852b15d8364203d7f432f026b621
xyryc <amdanikzz@gmail.com>
2024-10-02 22:39:35 +0600

    api setup and navbar added

---
+++ scripts/video.js
@@ -0,0 +1,12 @@
+const loadCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+};
+
+const displayCategories = (data) => {
+  console.log("catagories", data);
+};
+
+loadCategories();
