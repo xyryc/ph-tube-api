d28e997e062b7b22a0b96801532d4243eb3094bd
xyryc <amdanikzz@gmail.com>
2024-10-03 11:24:41 +0600

    conditional redering of verfied badge

--- scripts/video.js
+++ scripts/video.js
@@ -41,21 +41,29 @@ const displayVideos = (videos) => {
     const card = document.createElement("div");
     card.classList = "card card-compact ";
     card.innerHTML = `
-       <figure class='h-[200px]'>
+       <figure class='h-[200px] relative'>
         <img
           src=${video.thumbnail}
           class='h-full w-full object-cover'
           alt="Shoes" />
+
+          <span class='absolute bottom-2 right-2 bg-black rounded p-1 text-white'>${video.others.posted_date} </span>
         </figure>
       <div class="px-0 py-2 flex gap-2">
         <div>
-            <img class='w-10 h-10 rounded-full object-cover' src=${video.authors[0].profile_picture}/>
+            <img class='w-10 h-10 rounded-full object-cover' src=${
+              video.authors[0].profile_picture
+            }/>
         </div>
         <div>
             <h2 class='font-bold '>${video.title}</h2>
             <div class='flex items-center gap-2'>
             <p class='text-gray-400'>${video.authors[0].profile_name}</p>
-            <img class='w-5' src='https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png' />
+            ${
+              video.authors[0].verified === true
+                ? `<img class='w-5' src='https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png' />`
+                : ''
+            }
             </div>
             <p> ${video.others.views}</p>
         </div>
